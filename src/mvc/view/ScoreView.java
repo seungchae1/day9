d92b9ab0fd56e23c7eec07846fa240b0445e2762
박승채 <@DESKTOP-P53MBUO>
2022-01-13 15:46:54 +0900

    Score Project

---
+++ src/mvc/view/ScoreView.java
@@ -0,0 +1,34 @@
+package mvc.view;
+
+import java.util.ArrayList;
+
+import mvc.vo.StudentVO;
+
+public class ScoreView {
+	
+	private ArrayList<StudentVO> studentArrList;
+	
+	public ScoreView(ArrayList<StudentVO> studentArrList) {
+		this.studentArrList = studentArrList;
+	}
+	
+	public void print() {
+		System.out.println("===== 전체 학생 성적 정보 출력 =====");
+		for(int i=0; i<studentArrList.size(); i++) {
+			StudentVO s = studentArrList.get(i);	
+			int sum =clacSum(s);
+			double avg=calcAvg(sum);
+			System.out.printf("* %d  %s  %d  %d  %d  %d  %d  합계 : %d  평균 : %.2f\n", 
+								s.getStuID(), s.getName(), s.getKor(), s.getMath(), s.getEng(), s.getScience(), s.getHistory(), 
+								sum, avg);
+		}
+	}
+	
+	public int clacSum(StudentVO s) {
+		return s.getKor()+s.getMath()+s.getEng()+s.getScience()+s.getHistory();
+	}
+	
+	public double calcAvg(int sum) {
+		return sum/5.0;
+	}
+}
