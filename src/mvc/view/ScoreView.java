2bc81ff4f17848b00ee5e1f181d0aac4a7eaa3b1
박승채 <@DESKTOP-P53MBUO>
2022-01-14 13:58:08 +0900

    mvc

--- src/mvc/view/ScoreView.java
+++ src/mvc/view/ScoreView.java
@@ -18,7 +18,7 @@ public class ScoreView {
 			StudentVO s = studentArrList.get(i);	
 			int sum =clacSum(s);
 			double avg=calcAvg(sum);
-			System.out.printf("* %d  %s  %d  %d  %d  %d  %d  합계 : %d  평균 : %.2f\n", 
+			System.out.printf("* %d  %s  국어 : %d  수학 : %d  영어 : %d  과학 : %d  역사 : %d  합계 : %d  평균 : %.2f\n", 
 								s.getStuID(), s.getName(), s.getKor(), s.getMath(), s.getEng(), s.getScience(), s.getHistory(), 
 								sum, avg);
 		}
